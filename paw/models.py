52826d38224bf6991a3df1dc150a8efec8195248
Maxime Lapointe <maxx@themaxx.ca>
2017-07-08 19:38:20 -0400

    wip

--- paw/models.py
+++ paw/models.py
@@ -4,18 +4,17 @@ import uuid
 from inspect import getmembers, isfunction
 from multiprocessing import Pool, Queue, Process, current_process
 import os
-
+import datetime
 # noinspection PyPackageRequirements
 from azure.storage.queue import QueueService
 # noinspection PyPackageRequirements
-from azure.storage.table import TableService
+from azure.storage.table import TableService, Entity
 import traceback
 
 
 class Worker(Process):
     def __init__(self, local_queue, queue_service, queue_name,
-                 table_service,
-                 tasks):
+                 table_service, table_name, tasks):
         super(Worker, self).__init__()
 
         self.local_queue = local_queue
@@ -25,7 +24,7 @@ class Worker(Process):
         self.tasks = tasks
         self.msg = None
 
-    def start_working(self):
+    def run(self):
         print('STARTING worker PID: {}'.format(os.getpid()))
         while True:
             self.msg = self.local_queue.get(True)
@@ -33,17 +32,16 @@ class Worker(Process):
             result = None
 
             try:
-                result = self.run()
+                result = self.exec()
             except Exception as e:
                 exception_message = e
             finally:
-                self.delete_message()
-                self.log_to_table(result, exception_message)
+                log_to_table(result, exception_message)
                 print(exception_message, result,
                       'TB\n:', traceback.format_exc())
             time.sleep(5)
 
-    def run(self):
+    def exec(self):
         result = None
         exception = None
         print(current_process().name, self.msg.get('task_name'))
@@ -63,75 +61,22 @@ class Worker(Process):
         except Exception as e:
             exception = str(e)
         finally:
-            self.delete_message()
-            self.log_to_table(result, exception)
-
-    def delete_message(self):
-        self.qs.delete_message(self.azure_queue_name, self.msg['azure_id'],
-                               self.msg['pop_receipt'])
-        print('DELETED!!!!!!!!!!!!!!!!!!!!')
-
-
-    def log_to_table(self, result, exception):
-        pass
-
-
-class _Worker:
-    def __init__(self, local_queue, queue_service, queue_name, table_service,
-                 tasks):
-        self.local_queue = local_queue
-        self.qs = queue_service
-        self.ts = table_service
-        self.azure_queue_name = queue_name
-        self.tasks = tasks
-
-    def start_working(self):
-        print('STARTING worker PID: {}'.format(os.getpid()))
-
-        while True:
-            msg = self.local_queue.get(True)
-            exception_message = None
-            result = None
-
-            try:
-                result = self.run(msg)
-            except Exception as e:
-                exception_message = e
-            finally:
-                self.delete_message(msg)
-                self.log_to_table(msg, result, exception_message)
-                print(exception_message, result,
-                      'TB\n:', traceback.format_exc())
-            time.sleep(5)
-
-    def run(self, msg):
-        result = None
-        exception = None
-        # message = msg['content
-
-        func = self.tasks.get(msg['task_name'])
-        if not func:
-            raise Exception(
-                '{} is not a registered task.'.format(msg['task_name']))
-
-        try:
-            if msg['args']:
-                result = func(*msg['args'])
-            elif msg['kwargs']:
-                result = func(**msg['kwargs'])
+            # self.delete_message()
+            if exception:
+                status = 'failed'
             else:
-                result = func()
-        except Exception as e:
-            exception = str(e)
-        finally:
-            self.delete_message(msg)
-            self.log_to_table(msg, result, exception)
+                status = 'success'
 
-    def delete_message(self, msg):
-        pass
+            log_to_table(self.ts, status, self.msg['id'], result=result, exception=exception)
 
-    def log_to_table(self, msg, result, exception):
-        pass
+    # def delete_message(self):
+    #     self.qs.delete_message(self.azure_queue_name, self.msg['azure_id'],
+    #                            self.msg['pop_receipt'])
+    #     print('DELETED!!!!!!!!!!!!!!!!!!!!')
+
+
+    # def log_to_table(self, result, exception):
+    #     pass
 
 
 class Message:
@@ -158,10 +103,10 @@ class Message:
 
 class MainPawWorker:
     def __init__(self, azure_storage_name, azure_storage_private_key,
-                 azure_storage_queue_name, tasks_module, workers):
+                 azure_queue_name, azure_table_name, tasks_module, workers):
         self.account_name = azure_storage_name
         self.account_key = azure_storage_private_key
-        self.queue_name = azure_storage_queue_name
+        self.queue_name = azure_queue_name
         self.tasks_module = tasks_module
         self.workers = workers
         self.queue_service = QueueService(account_name=self.account_name,
@@ -176,7 +121,7 @@ class MainPawWorker:
             table_service=self.table_service,
             tasks=self._load_tasks()
         )
-        self.pool = Pool(self.workers, self.worker_process.start_working, ())
+        self.pool = Pool(self.workers, self.worker_process.run, ())
 
     def _load_tasks(self):
         tasks = dict(
@@ -207,12 +152,10 @@ class MainPawWorker:
                     )
                     if new_msg:
                         msg = new_msg[0]
-                        new_content = {
-                            'pop_receipt': msg.pop_receipt,
-                            'azure_id': msg.id
-                        }
-                        content = {**json.loads(msg.content), **new_content}
-                        self.local_queue.put_nowait(content)
+                        content = json.loads(msg.content)
+                        self.local_queue.put_nowait(json.loads(msg.content))
+                        self.queue_service.delete_message(
+                            self.queue_name, msg.id, msg.pop_receipt)
                         print('\tadded: {}'.format(content))
 
                 except Exception as e:
@@ -223,6 +166,26 @@ class MainPawWorker:
                     print(e)
 
             time.sleep(5)
+
+
+def log_to_table(ts, status, job_id, result=None, exception=None, update=False):
+    entity = Entity()
+
+    if not update:
+        entity.created = datetime.datetime.utcnow()
+    else:
+        entity.completed = datetime.datetime.utcnow()
+
+    entity.status = status
+    entity.job_id = job_id
+    entity.result = result
+    entity.result = exception
+
+
+
+
+
+
 
 
 def task(description=''):
